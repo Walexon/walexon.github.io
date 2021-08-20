927d7bc770b11b041e148da871014d8cd7db4e2f
Walexon <76918300+Walexon@users.noreply.github.com>
2021-08-20 20:51:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+(
+    var loop_the_loop = true;
+    loop_the_loop;         // check: only loop if true!
+    loop_the_loop = false // do this after each run
+    )
+{ console.log("I'm a pretty useless loop"); }
+
+(var i = 1; i <= 10; i++) {
+    console.log("Walexon");
+  }
